b9fe60f63eb547e1aaf6d5a3a5da13f8075a2f57
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-22 16:23:15 +0000

    Apply C# standard

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -2,112 +2,112 @@ namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int _p1Point;
+        private int _p2Point;
 
-        private string p1res = "";
-        private string p2res = "";
-        private readonly string player1Name;
-        private readonly string player2Name;
+        private string _p1Res = "";
+        private string _p2Res = "";
+        private readonly string _player1Name;
+        private readonly string _player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
-            p1point = 0;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
+            _p1Point = 0;
         }
 
         public void WonPoint(string player)
         {
-            if (player == player1Name)
-                p1point++;
+            if (player == _player1Name)
+                _p1Point++;
             else
-                p2point++;
+                _p2Point++;
         }
 
         public string GetScore()
         {
             var score = "";
-            if (p1point == p2point && p1point < 3)
+            if (_p1Point == _p2Point && _p1Point < 3)
             {
-                if (p1point == 0)
+                if (_p1Point == 0)
                     score = "Love";
-                if (p1point == 1)
+                if (_p1Point == 1)
                     score = "Fifteen";
-                if (p1point == 2)
+                if (_p1Point == 2)
                     score = "Thirty";
                 score += "-All";
             }
-            if (p1point == p2point && p1point > 2)
+            if (_p1Point == _p2Point && _p1Point > 2)
                 score = "Deuce";
 
-            if (p1point > 0 && p2point == 0)
+            if (_p1Point > 0 && _p2Point == 0)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
+                if (_p1Point == 1)
+                    _p1Res = "Fifteen";
+                if (_p1Point == 2)
+                    _p1Res = "Thirty";
+                if (_p1Point == 3)
+                    _p1Res = "Forty";
 
-                p2res = "Love";
-                score = p1res + "-" + p2res;
+                _p2Res = "Love";
+                score = _p1Res + "-" + _p2Res;
             }
-            if (p2point > 0 && p1point == 0)
+            if (_p2Point > 0 && _p1Point == 0)
             {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
+                if (_p2Point == 1)
+                    _p2Res = "Fifteen";
+                if (_p2Point == 2)
+                    _p2Res = "Thirty";
+                if (_p2Point == 3)
+                    _p2Res = "Forty";
 
-                p1res = "Love";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
+                _p1Res = "Love";
+                score = _p1Res + "-" + _p2Res;
             }
 
-            if (p1point > p2point && p2point >= 3)
+            if (_p1Point > _p2Point && _p1Point < 4)
             {
-                score = $"Advantage {player1Name}";
+                if (_p1Point == 2)
+                    _p1Res = "Thirty";
+                if (_p1Point == 3)
+                    _p1Res = "Forty";
+                if (_p2Point == 1)
+                    _p2Res = "Fifteen";
+                if (_p2Point == 2)
+                    _p2Res = "Thirty";
+                score = _p1Res + "-" + _p2Res;
+            }
+            if (_p2Point > _p1Point && _p2Point < 4)
+            {
+                if (_p2Point == 2)
+                    _p2Res = "Thirty";
+                if (_p2Point == 3)
+                    _p2Res = "Forty";
+                if (_p1Point == 1)
+                    _p1Res = "Fifteen";
+                if (_p1Point == 2)
+                    _p1Res = "Thirty";
+                score = _p1Res + "-" + _p2Res;
             }
 
-            if (p2point > p1point && p1point >= 3)
+            if (_p1Point > _p2Point && _p2Point >= 3)
             {
-                score = $"Advantage {player2Name}";
+                score = $"Advantage {_player1Name}";
             }
 
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
+            if (_p2Point > _p1Point && _p1Point >= 3)
             {
-                score = $"Win for {player1Name}";
+                score = $"Advantage {_player2Name}";
             }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
+
+            if (_p1Point >= 4 && _p2Point >= 0 && (_p1Point - _p2Point) >= 2)
             {
-                score = $"Win for {player2Name}";
+                score = $"Win for {_player1Name}";
+            }
+            if (_p2Point >= 4 && _p1Point >= 0 && (_p2Point - _p1Point) >= 2)
+            {
+                score = $"Win for {_player2Name}";
             }
             return score;
         }

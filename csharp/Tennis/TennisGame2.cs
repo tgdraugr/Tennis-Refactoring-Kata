dec168f1f3decc5f85131981e56e07ac85c87c8d
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-22 16:22:42 +0000

    Substitute local variable for instance variable, wherever possible

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -7,8 +7,8 @@ namespace Tennis
 
         private string p1res = "";
         private string p2res = "";
-        private string player1Name;
-        private string player2Name;
+        private readonly string player1Name;
+        private readonly string player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
@@ -19,7 +19,7 @@ namespace Tennis
 
         public void WonPoint(string player)
         {
-            if (player == "player1")
+            if (player == player1Name)
                 p1point++;
             else
                 p2point++;
@@ -93,21 +93,21 @@ namespace Tennis
 
             if (p1point > p2point && p2point >= 3)
             {
-                score = "Advantage player1";
+                score = $"Advantage {player1Name}";
             }
 
             if (p2point > p1point && p1point >= 3)
             {
-                score = "Advantage player2";
+                score = $"Advantage {player2Name}";
             }
 
             if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
             {
-                score = "Win for player1";
+                score = $"Win for {player1Name}";
             }
             if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
             {
-                score = "Win for player2";
+                score = $"Win for {player2Name}";
             }
             return score;
         }

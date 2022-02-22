e57fffa927bd0cc7c09fdaeebfdbb28d0f478b20
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-22 16:17:55 +0000

    Remove of single used methods

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -13,8 +13,16 @@ namespace Tennis
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
+            p1point = 0;
+        }
+
+        public void WonPoint(string player)
+        {
+            if (player == "player1")
+                p1point++;
+            else
+                p2point++;
         }
 
         public string GetScore()
@@ -103,41 +111,6 @@ namespace Tennis
             }
             return score;
         }
-
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
-        }
-
-        public void WonPoint(string player)
-        {
-            if (player == "player1")
-                P1Score();
-            else
-                P2Score();
-        }
-
     }
 }
 
